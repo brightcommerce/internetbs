525c663f012858d01b449c17474f0c20d031fe37
kodemunki <hello@kodemunki.com>
2015-11-24 07:58:28 +1000

    Added DomainAvailability#available? to return Boolean in addition to #status property.

--- lib/internetbs/domain_availability.rb
+++ lib/internetbs/domain_availability.rb
@@ -1,11 +1,16 @@
 module InternetBS
   class DomainAvailability < Base
-    attribute :domain,                 String
-    attribute :max_reg_period,         String
-    attribute :min_reg_period,         String
-    attribute :private_whois_allowed,  Boolean
-    attribute :realtime_registration,  Boolean
-    attribute :registrar_lock_allowed, Boolean
+    attribute :available,               Boolean, :default => false
+    attribute :domain,                  String
+    attribute :max_registration_period, String
+    attribute :min_registration_period, String
+    attribute :private_whois_allowed,   Boolean
+    attribute :realtime_registration,   Boolean
+    attribute :registrar_lock_allowed,  Boolean
+    
+    def available?
+      @available
+    end
     
     def fetch
       ensure_attribute_has_value :domain
@@ -21,11 +26,16 @@ module InternetBS
         
         @status = hash['status']
         @transaction_id = hash['transactid']
-        @max_registration_period = hash['maxregperiod']
-        @min_registration_period = hash['minregperiod']
-        @private_whois_allowed   = hash['privatewhoisallowed']  == 'YES' ? true : false
-        @realtime_registration   = hash['realtimeregistration'] == 'YES' ? true : false
-        @registrar_lock_allowed  = hash['registrarlockallowed'] == 'YES' ? true : false
+        
+        if @status == 'AVAILABLE'
+          @available = true
+        
+          @max_registration_period = hash['maxregperiod']
+          @min_registration_period = hash['minregperiod']
+          @private_whois_allowed   = hash['privatewhoisallowed']  == 'YES' ? true : false
+          @realtime_registration   = hash['realtimeregistration'] == 'YES' ? true : false
+          @registrar_lock_allowed  = hash['registrarlockallowed'] == 'YES' ? true : false
+        end
         
         return true
       else
